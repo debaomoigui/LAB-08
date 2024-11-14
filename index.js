6c3f765da1827bc16e08644a42811fbc09a1738e
debaomoigui <omoiguideba@gmail.com>
2024-11-14 11:43:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+var name = "Deba";
+
+var age = "18";
+
+console.log("Name:", name);
+
+console.log("Age:", age);
+
+let course = "Math";
+
+let year = "2024";
+
+let student = "yes";
+
+let city;
+
+console.log("Course:", course);
+
+console.log("Year:", year);
+
+console.log("Student?", student);
+
+console.log("City:", city);
+
+
+let RollNumber = "15";
+
+if (RollNumber >10) {console.log("The roll number is greater than 10")}
+
+let isMember = "true";
+
+if(isMember = "true") {console.log("Member discount applied")}
+
+let StartValue = "50";
+
+if(StartValue >0) {console.log("The Start Value is positive")}
+
+if(StartValue =0) {console.log("The Start Value is zero")}
+
+if(StartValue <0) {console.log("The Start Value is negative")}
+
+
+let a = "10";
+
+let b = "20";
+
+console.log("a==b:", a==b);
+console.log("a===b:", a===b);
+console.log("a!=b:", a!=b);
+console.log("a>b:", a>b);
+console.log("a<b:", a<b);
+console.log("a>=b:", a>=b);
+console.log("a<=b:", a<=b);
+
+
+let FirstName = "Deba";
+
+let LastName = "Omoigui";
+
+let age2 = "17";
+
+console.log("Hello, ", FirstName, LastName, ". Your are", age2 ,"years old.");
+
+let num = "50";
+
+if( num>0 && num<100) {console.log("The number is within range");}
+
+
+let hasCar = "yes";
+
+let hasLicense = "yes";
+
+let hasInsurance = "yes";
+
+if(hasCar && hasLicense && hasInsurance) {console.log("You can drive legally");
+} else {
+    console.log("Check your driving eligibility");
+}
+
+let num2 = "21";
+
+if(num2 % 2 === 0) {console.log("The number is even")}
+else {
+    console.log("The number is odd")
+}
+
+let score = "85";
+
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 80 && score <= 89) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
